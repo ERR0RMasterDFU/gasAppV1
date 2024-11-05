cb529e0f0a4564246ac0d2ea67a7fe03f2d703e2
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-11-05 22:22:28 +0100

    Jubilación

--- gasAppV1/src/app/app.module.ts
+++ gasAppV1/src/app/app.module.ts
@@ -5,22 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GasolineraListComponent } from './components/gasolinera-list/gasolinera-list.component';
-import { AsideComponent } from './shared/aside/aside.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { provideHttpClient } from '@angular/common/http';
+import { NavComponent } from './shared/nav/nav.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
     GasolineraListComponent,
-    AsideComponent,
-    HeaderComponent
+    NavComponent,
+    HeaderComponent,
+    NavComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(), 
+    provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
